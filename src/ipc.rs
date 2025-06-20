860cf57fd4b3d187bb6768f7e29a993282d1381c
David G Rosenberg <targendaz2@me.com>
2025-06-20 00:04:20 -0400

    refactor: update Socket struct fields for clarity and consistency

--- src/ipc.rs
+++ src/ipc.rs
@@ -98,41 +98,50 @@ pub struct Socket {
     #[serde(rename = "SockProtocol")]
     pub protocol: Option<SocketProtocol>,
 
-    /// This optional key implies SockFamily is set to "Unix". It specifies
-    /// the path to connect(2) or bind(2) to.
-    sock_path_name: Option<String>,
+    /// Specifies the path to `connect(2)` or `bind(2)` to.
+    ///
+    /// Implies [`family`](Self::family) is set to
+    /// [`Unix`](SocketFamily::Unix).
+    #[serde(rename = "SockPathName")]
+    pub path_name: Option<String>,
 
-    /// This optional key is a variant of SockPathName. Instead of binding
-    /// to a known path, a securely generated socket is created and the
-    /// path is assigned to the environment variable that is inherited by
-    /// all jobs spawned in the job's context.
-    secure_socket_with_key: Option<String>,
+    /// A variant of [`path_name`](Self::path_name). Instead of binding to a
+    /// known path, a securely generated socket is created and the path is
+    /// assigned to the environment variable that is inherited by all jobs
+    /// spawned in the job's context.
+    pub secure_socket_with_key: Option<String>,
 
-    /// This optional key specifies the user ID that should be the domain
-    /// socket's owner.
-    sock_path_owner: Option<u32>,
+    /// The user ID that should be the domain socket's owner.
+    #[serde(rename = "SockPathOwner")]
+    pub path_owner: Option<u32>,
 
-    /// This optional key specifies the group ID that should be set as the
-    /// domain socket's group.
-    sock_path_group: Option<u32>,
+    /// The group ID that should be set as the domain socket's group.
+    #[serde(rename = "SockPathGroup")]
+    pub path_group: Option<u32>,
 
-    /// This optional key specifies the mode of the socket. Known bug:
-    /// Property lists don't support octal, so please convert the value to
-    /// decimal
-    sock_path_mode: Option<u32>,
+    /// The mode of the socket.
+    ///
+    /// <div class="warning">
+    ///     Known bug: Property lists don't support octal, so please convert
+    ///     the value to decimal.
+    /// </div>
+    #[serde(rename = "SockPathMode")]
+    pub path_mode: Option<f32>,
 
-    /// This optional key can be used to request that the service be
-    /// registered with the the Bonjour subsystem. If the value is boolean,
-    /// the service name is inferred from the SockServiceName.
-    bonjour: Option<Bonjour>,
+    /// Request that the service be registered with the the Bonjour subsystem.
+    ///
+    /// If the value is boolean, the service name is inferred from
+    /// [`service_name`](Self::service_name).
+    pub bonjour: Option<Bonjour>,
 
-    /// This optional key can be used to request that the datagram socket
-    /// join a multicast group. If the value is a hostname, then
-    /// getaddrinfo(3) will be used to join the correct multicast address
-    /// for a given socket family.  If an explicit IPv4 or IPv6 address is
-    /// given, it is required that the SockFamily family also be set,
-    /// otherwise the results are undefined.
-    multicast_group: Option<String>,
+    /// Request that the datagram socket join a multicast group.
+    ///
+    /// If the value is a hostname, then `getaddrinfo(3)` will be used to join
+    /// the correct multicast address for a given socket family.  If an
+    /// explicit IPv4 or IPv6 address is given, it is required that
+    /// [`family`](Self::family) also be set, otherwise the results are
+    /// undefined.
+    pub multicast_group: Option<String>,
 }
 
 #[derive(Clone, Deserialize, Serialize)]
@@ -147,6 +156,7 @@ pub enum SocketFamily {
     IPv4,
     IPv6,
     IPv4v6,
+    Unix,
 }
 
 #[derive(Clone, Deserialize, Serialize)]
